2417988cda4d6786d60d7ed61af1796848c94bc5
Batoul <batoulalso68@gmail.com>
2026-08-17 14:33:04 -0400

    useState

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,11 @@
-
-import ProfilePicture from './ProfilePicture.jsx';
+import MyComponent from "./MyComponent.jsx";
+import Counter from "./Counter.jsx";
 
 function App() {
 
   return(
     <>
-      <ProfilePicture />
+      <Counter />
     </>
   );
 }
