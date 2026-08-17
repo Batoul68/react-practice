06f9f54da18b0fbb3413641857ede07ab71f3b46
Batoul <batoulalso68@gmail.com>
2026-08-17 13:53:55 -0400

    click events

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,11 @@
-import List from './List.jsx'
+
+import ProfilePicture from './ProfilePicture.jsx';
 
 function App() {
 
-  const animes = [{id:1, name:"My Hero Academia", seasons:8}, 
-                {id:2, name:"Jujutsu Kaisen", seasons:3}, 
-                {id:3, name:"Haikyuu", seasons:5}];
-
-  const shows = [{id:6, name:"Shameless", seasons:8}, 
-                {id:7, name:"The Vampire Diaries", seasons:9}, 
-                {id:8, name:"The Originals", seasons:7}];
   return(
     <>
-      {animes.length > 0 && <List items={animes} category="animes"/>}
-      {shows.length > 0 && <List items={shows} category="shows" />}
+      <ProfilePicture />
     </>
   );
 }
---
+++ my-react-app/src/ProfilePicture.jsx
@@ -0,0 +1,14 @@
+
+function ProfilePicture() {
+
+  const imageUrl = `./src/assets/cat.png`;
+
+  const handleClick = (e) => {
+    e.target.style.display = "none";
+  }
+
+  return(
+    <img src={imageUrl} onClick={(e) => handleClick(e)}></img>
+  );
+}
+export default ProfilePicture
